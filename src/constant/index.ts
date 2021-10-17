dfcbd744927b18dd7a3b338e3d9b946986d24d5e
castlemo <eodah4115@gmail.com>
2021-10-17 22:58:42 +0900

    backend url Add

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,4 @@
 export const API_URL =
   process.env.REACT_APP_MODE === 'production'
-    ? 'https://api.mathpang.com/graphql'
+    ? 'https://tiry-backend.herokuapp.com/graphql'
     : 'http://localhost:9000/graphql';
