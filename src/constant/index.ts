8cb5158514bb587f0a7b6a176622be6fadc98ce7
castlemo <eodah4115@gmail.com>
2021-10-18 17:19:02 +0900

    Fix API_URL logic

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,4 +1,3 @@
-export const API_URL =
-  process.env.REACT_APP_MODE === 'production'
-    ? 'https://tiry-backend.herokuapp.com/graphql'
-    : 'http://localhost:9000/graphql';
+export const API_URL = !(process.env.REACT_APP_MODE === 'local')
+  ? 'https://tiry-backend.herokuapp.com/graphql'
+  : 'http://localhost:9000/graphql';
